eca1ae8ba1137e4fb2e9c0f777e85f3c5cb4ea4b
Andy Hubert <andy.hubert+8@gmail.com>
2017-12-04 11:40:45 +0200

    Installed material-ui

--- widget/src/App.js
+++ widget/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import logo from './logo.svg'
 import './App.css'
 
@@ -16,15 +18,17 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <MuiThemeProvider>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        </MuiThemeProvider>
       </ApolloProvider>
     )
   }
--- widget/src/index.js
+++ widget/src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+injectTapEventPlugin()
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
