af7ce751b5af99da7788265b7fa3fc3a2ca24853
Andy Hubert <andy.hubert+8@gmail.com>
2017-12-05 11:54:54 +0200

    put widget script builds in folder

--- deployScript.js
+++ deployScript.js
@@ -1,3 +1,3 @@
 const shell = require('shelljs')
 
-shell.exec(`uglifyjs widget-script.js -c -m -o widget-script-${Date.now()}.js`)
+shell.exec(`uglifyjs widget-script.js -c -m -o ./widget-script-builds/widget-script-${Date.now()}.js`)
