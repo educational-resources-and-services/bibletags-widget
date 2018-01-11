b19ff5a74af62b6e8763e268f5d8070bb31d6c69
Andy Hubert <andy.hubert+8@gmail.com>
2018-01-11 11:32:01 +0200

    Styling of entry word and details

--- widget/src/components/basic/EntryDetails.js
+++ widget/src/components/basic/EntryDetails.js
@@ -2,19 +2,50 @@ import React from 'react'
 import i18n from '../../utils/i18n.js'
 import styled from 'styled-components'
 
-// const StyledSomething = styled.div`
-//   height: 3px;
-// `
+import Icon from 'material-ui/Icon'
+import IconButton from 'material-ui/IconButton'
+import LibraryBooksIcon from 'material-ui-icons/LibraryBooks'
 
-class EntryDetails extends React.Component {
+const DetailsLine = styled.div`
+  padding-top: 10px;
+`
+
+const Definition = styled.div`
+  font-size: 15px;
+  font-weight: bold;
+  display: inline-block;
+  margin-right: 3px;
+`
+
+const PartOfSpeech = styled.div`
+  font-size: 14px;
+  display: inline-block;
+`
+
+const IconButtonStyled = styled(IconButton)`
+  margin: -100px 0 !important;
+  vertical-align: middle;
+`
+
+class EntryWord extends React.Component {
   render() {
     const { something } = this.props 
 
     return (
-      <div />
+      <DetailsLine>
+        <Definition>beginning</Definition>
+        <span> </span>
+        <PartOfSpeech>noun</PartOfSpeech>
+        <IconButtonStyled
+          aria-label="Lexicon"
+          onTouchTap={() => {}}
+        >
+          <LibraryBooksIcon />
+        </IconButtonStyled>
+      </DetailsLine>
     )
   }
 
 }
 
-export default EntryDetails
+export default EntryWord
--- widget/src/components/basic/EntryWord.js
+++ widget/src/components/basic/EntryWord.js
@@ -2,16 +2,64 @@ import React from 'react'
 import i18n from '../../utils/i18n.js'
 import styled from 'styled-components'
 
-// const StyledSomething = styled.div`
-//   height: 3px;
-// `
+import Icon from 'material-ui/Icon'
+import IconButton from 'material-ui/IconButton'
+import SearchIcon from 'material-ui-icons/Search'
+
+const WordLine = styled.div`
+  padding-right: 40px;
+`
+
+const Lemma = styled.div`
+  font-size: 17px;
+  display: inline-block;
+  margin-right: 3px;
+`
+
+const Pronounciation = styled.div`
+  font-size: 15px;
+  color: rgba(0,0,0,.5);
+  display: inline-block;
+  margin-right: 3px;
+`
+
+const Strongs = styled.div`
+  display: inline-block;
+  margin-right: 3px;
+`
+
+const Hits = styled.div`
+  font-size: 13px;
+  color: rgba(0,0,0,.5);
+  display: inline-block;
+`
+
+const IconButtonStyled = styled(IconButton)`
+  position: absolute !important;
+  top: 0;
+  right: 0;
+`
 
 class EntryWord extends React.Component {
   render() {
     const { something } = this.props 
 
     return (
-      <div />
+      <WordLine>
+        <Lemma>ראשית</Lemma>
+        <span> </span>
+        <Pronounciation>reshit</Pronounciation>
+        <span> </span>
+        <Strongs>H938</Strongs>
+        <span> </span>
+        <Hits>13x</Hits>
+        <IconButtonStyled
+          aria-label="Search"
+          onTouchTap={() => {}}
+        >
+          <SearchIcon />
+        </IconButtonStyled>
+      </WordLine>
     )
   }
 
